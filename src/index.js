9113e3962a13dca391a6c91b3577ee441e62f3fa
npecca <mark.karynos@gmail.com>
2023-08-21 00:01:49 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+  let stack = [];
+  let bracketsMap = bracketsConfig.reduce((acc, curr) => {
+    acc[curr[0]] = curr[1];
+    return acc
+  }, {});
+  for (let char of str) {
+    if (stack.length > 0 && char === bracketsMap[stack.slice(-1)]) {
+      stack.pop()
+    }
+    else {
+      stack.push(char);
+    }
+  }
+  return stack.length === 0;
 }
+module.exports = check;
